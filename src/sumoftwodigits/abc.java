96d4f465bc9a46d1ca0b961496a0d632333552fc
usharani4 <usharanidodda4@gmail.com>
2021-04-21 14:19:26 +0530

    sum file created

---
+++ src/sumoftwodigits/abc.java
@@ -0,0 +1,16 @@
+
+
+public class abc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10,b=2,res;
+System.out.println("a"+a);
+System.out.println("b"+b);
+res=a+b;
+System.out.println("res is"+res);
+	}
+}
+	}
+
+}
